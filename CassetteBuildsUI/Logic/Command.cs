e7e7b73289e4506be0c2cfead85cbb54e9cd1905
Kryzarel <8752418+Kryzarel@users.noreply.github.com>
2024-03-15 03:40:14 +0000

    Implemented reactive command to disable button when selected move is not valid. According to docs it should have worked by simply having a method prefixed with "Can" but ofc that doesn't work at all. Added implementation of Command which could be useful to replace/merge existing commands that I ended up not using right now.

---
+++ CassetteBuildsUI/Logic/Command.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Windows.Input;
+
+namespace CassetteBuildsUI.Logic
+{
+	public class Command : ICommand
+	{
+		public event EventHandler? CanExecuteChanged;
+
+		private readonly Action<object?> execute;
+		private readonly Func<object?, bool>? canExecute;
+
+		public Command(Action<object?> execute, Func<object?, bool>? canExecute = null, EventHandler? canExecuteChanged = null)
+		{
+			this.execute = execute;
+			this.canExecute = canExecute;
+			canExecuteChanged += InvokeCanExecuteChanged;
+		}
+
+		public bool CanExecute(object? parameter)
+		{
+			return canExecute?.Invoke(parameter) ?? true;
+		}
+
+		public void Execute(object? parameter)
+		{
+			execute?.Invoke(parameter);
+		}
+
+		private void InvokeCanExecuteChanged(object? sender, EventArgs args)
+		{
+			CanExecuteChanged?.Invoke(sender, args);
+		}
+	}
+}
--- CassetteBuildsUI/ViewModels/MonsterSearchViewModel.cs
+++ CassetteBuildsUI/ViewModels/MonsterSearchViewModel.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Reactive;
 using CassetteBuilds.Code.Core;
 using CassetteBuilds.Code.Data;
 using CassetteBuildsUI.Models;
@@ -16,11 +17,13 @@ namespace CassetteBuildsUI.ViewModels
 		private string? selectedMove;
 		public string? SelectedMove { get => selectedMove; set => this.RaiseAndSetIfChanged(ref selectedMove, value); }
 
-		public ReadOnlyObservableCollection<MoveModel> MovesFilter { get; }
-		public ReadOnlyObservableCollection<MonsterModel> Results { get; }
 		public static IReadOnlyList<string> MoveNames { get; }
 		public static IReadOnlyList<MoveModel> Moves => Logic.Database.Moves;
 		public static IReadOnlyList<MonsterModel> Monsters => Logic.Database.Monsters;
+
+		public ReadOnlyObservableCollection<MoveModel> MovesFilter { get; }
+		public ReadOnlyObservableCollection<MonsterModel> Results { get; }
+		public ReactiveCommand<string?, Unit> AddMoveCommand { get; }
 
 		private readonly ObservableCollection<MoveModel> movesFilter = [];
 		private readonly ObservableCollection<MonsterModel> results = [];
@@ -43,21 +46,33 @@ namespace CassetteBuildsUI.ViewModels
 			MovesFilter = new(movesFilter);
 			Results = new(results);
 			RecalculateResults();
+
+			IObservable<bool> canAddMove = this.WhenAnyValue(x => x.SelectedMove, CanAddMove);
+			AddMoveCommand = ReactiveCommand.Create<string?>(AddMove, canAddMove);
 		}
 
-		public bool TryAddMove(string? moveName)
+		public bool CanAddMove(string? moveName)
 		{
 			if (count >= moveIndexes.Length || moveName == null)
 				return false;
-			if (!MoveFinder.TryGetMoveIndex(moveName, out int index))
+			if (!MoveFinder.TryGetMoveIndex(moveName, out _))
 				return false;
-			return TryAddMove(Moves[index]);
+			return true;
 		}
 
-		public bool TryAddMove(MoveModel? move)
+		public void AddMove(string? moveName)
+		{
+			if (count >= moveIndexes.Length || moveName == null)
+				return;
+			if (!MoveFinder.TryGetMoveIndex(moveName, out int index))
+				return;
+			AddMove(Moves[index]);
+		}
+
+		public void AddMove(MoveModel? move)
 		{
 			if (count >= moveIndexes.Length || move == null)
-				return false;
+				return;
 
 			SearchText = null;
 			SelectedMove = null;
@@ -65,17 +80,16 @@ namespace CassetteBuildsUI.ViewModels
 			moveIndexes[count++] = move.Index;
 			movesFilter.Add(move);
 			RecalculateResults();
-			return true;
 		}
 
-		public bool TryRemoveMove(MoveModel? move)
+		public void RemoveMove(MoveModel? move)
 		{
 			if (count <= 0 || move == null)
-				return false;
+				return;
 
 			int index = movesFilter.IndexOf(move);
 			if (index < 0 || index > count)
-				return false;
+				return;
 
 			movesFilter.RemoveAt(index);
 			count--;
@@ -84,7 +98,6 @@ namespace CassetteBuildsUI.ViewModels
 				Array.Copy(moveIndexes, index + 1, moveIndexes, index, count - index);
 			}
 			RecalculateResults();
-			return true;
 		}
 
 		private void RecalculateResults()
